d113c48266874122a030964809d5079636859b5b
TDL3 <29535697+TDL3@users.noreply.github.com>
2020-11-15 14:17:18 +0800

    Feature: Auto enable bbr algorithm

--- Trojan-GO/install.sh
+++ Trojan-GO/install.sh
@@ -73,12 +73,12 @@ apt-key fingerprint ABF5BD827BD9BF62
 apt update
 apt install nginx -y
 
-blue "Config nginx"
+blue "Config nginx."
 mkdir -p /var/www/html
 unzip -d /var/www/html $payload_dir/trojan.zip
 
 
-blue "Install certbot"
+blue "Install certbot."
 # Install this after nginx is necessary because python-certbot-nginx depends on nginx.
 apt install certbot python-certbot-nginx -y
 blue "Obtain tls certificate from Let's Encrypt."
@@ -86,7 +86,7 @@ systemctl start nginx
 certbot certonly --nginx -d $domain --register-unsafely-without-email --agree-tos
 
 
-blue "Install Trojan-GO"
+blue "Install Trojan-GO."
 source $payload_dir/trojan_go_update.sh
 cp $payload_dir/trojan_go_update.sh $trojan_dir
 
@@ -100,7 +100,7 @@ green " ============================== "
 read -p "Enter trojan-go password here: " password
 sed -i "s/PASSWORD/$password/" $payload_dir/config.json
 
-blue "Config Services"
+blue "Config Services."
 cp $payload_dir/trojan-go* /etc/systemd/system/
 cp $payload_dir/config.json $trojan_dir
 cp $payload_dir/trojan.conf /etc/nginx/conf.d/
@@ -108,12 +108,21 @@ cp $payload_dir/trojan.conf /etc/nginx/conf.d/
 mv /etc/nginx/conf.d/default.conf /etc/nginx/conf.d/default.conf.bak
 
 blue "Enable BBR"
+if [[ $(lsmod | grep tcp_bbr]) == "" ]]; then
+    yellow "bbr mod not running, enabling."
+    echo net.core.default_qdisc=fq >> /etc/sysctl.conf
+    echo net.ipv4.tcp_congestion_control=bbr >> /etc/sysctl.conf
+    sysctl -p
+    sysctl net.ipv4.tcp_available_congestion_control
+else 
+    green "bbr mod appears running."
+fi
 
-
-blue "Enable services"
+blue "Enable services."
 systemctl enable nginx trojan-go trojan-go-update.timer
 systemctl restart nginx
 systemctl start trojan-go trojan-go-update.timer
 systemctl status nginx 
 systemctl status trojan-go
-bold "Trojan-GO installation finished"
+lsmod | grep tcp_bbr
+bold "Trojan-GO installation finished."
