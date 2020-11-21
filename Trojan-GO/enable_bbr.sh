8b3463850fb6226cccf4b58ace6d581ca1c9ba00
TDL3 <29535697+TDL3@users.noreply.github.com>
2020-11-21 11:21:47 +0800

    rename

--- Trojan-GO/enable_bbr.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo net.core.default_qdisc=fq >> /etc/sysctl.conf
-echo net.ipv4.tcp_congestion_control=bbr >> /etc/sysctl.conf
-sysctl -p
-sysctl net.ipv4.tcp_available_congestion_control
-# 如果结果是这样 
-# sysctl net.ipv4.tcp_available_congestion_control
-# net.ipv4.tcp_available_congestion_control = bbr cubic reno
-# 就开启了。 执行 lsmod | grep bbr ，以检测 BBR 是否开启。
