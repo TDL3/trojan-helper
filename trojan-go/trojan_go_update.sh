adbfe2ceece24d8dd25f0608fec08f93861d4822
TDL3 <29535697+TDL3@users.noreply.github.com>
2020-11-21 20:45:38 +0800

    minor fix

--- trojan-go/trojan_go_update.sh
+++ trojan-go/trojan_go_update.sh
@@ -40,6 +40,10 @@ filename=$(basename "$url")
 # This regex matches version strings like x.x.x 
 regex="[0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2}"
 local_version=$($trojan_dir/trojan-go --version | head -1 | grep -oP $regex)
+if [[ -z "$local_version" ]]; then
+    red "Can not get local version of trojan"
+    local_version="0.0.0"
+fi
 remote_version=$(echo $url | grep -oP $regex)
 blue "Local version: "$local_version
 blue "Remote version: "$remote_version
