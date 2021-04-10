ca22fd729fe537f219a2032a52442beb60c9cbd3
TDL3 <29535697+TDL3@users.noreply.github.com>
2021-04-10 14:46:21 +0800

    Fix: nginx install script

--- trojan-gfw/install.sh
+++ trojan-gfw/install.sh
@@ -79,9 +79,23 @@ unzip -d $payload_dir $payload
 
 
 blue "Install the latest stable version of nginx."
+# Set up the apt repository for stable nginx packages
 echo "deb http://nginx.org/packages/debian `lsb_release -cs` nginx" | tee /etc/apt/sources.list.d/nginx.list
-curl -fsSL https://nginx.org/keys/nginx_signing.key | sudo apt-key add -
-apt-key fingerprint ABF5BD827BD9BF62
+# Set up repository pinning to prefer our packages over distribution-provided ones
+echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n" | tee /etc/apt/preferences.d/99nginx
+# Next, import an official nginx signing key so apt could verify the packages authenticity. Fetch the key
+curl -o /tmp/nginx_signing.key https://nginx.org/keys/nginx_signing.key
+# Verify that the downloaded file contains the proper key
+gpg --dry-run --quiet --import --import-options import-show /tmp/nginx_signing.key
+
+yellow "The output should contain the full fingerprint 573BFD6B3D8FBC641079A6ABABF5BD827BD9BF62 as follows"
+green "
+pub   rsa2048 2011-08-19 [SC] [expires: 2024-06-14]
+      573BFD6B3D8FBC641079A6ABABF5BD827BD9BF62
+uid                      nginx signing key <signing-key@nginx.com>
+"
+# Finally, move the key to apt trusted key storage (note the "asc" file extension change)
+mv /tmp/nginx_signing.key /etc/apt/trusted.gpg.d/nginx_signing.asc
 apt update
 apt install nginx -y
 
